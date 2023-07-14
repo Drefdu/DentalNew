f94e3a3649fd06d60208621e4c47a1003b53b7fa
Eduardo <jesuseduardoarciniegamoreno@gmail.com>
2023-07-13 22:14:50 -0600

    Ultimos cambios

--- src/app/tratamientos/perfi/perfi.page.ts
+++ src/app/tratamientos/perfi/perfi.page.ts
@@ -9,48 +9,75 @@ import { CameraService } from 'src/app/services/camera.service';
   styleUrls: ['./perfi.page.scss'],
 })
 export class PerfiPage implements OnInit {
-  selectTabs = 'galeria';
+  selectTabs = 'datos';
   isModalOpen = false;
+  presentingElement: any = null;
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
   }
 
-  constructor(private activatedRoute: ActivatedRoute, private dataService: DataService, private camera: CameraService) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private dataService: DataService,
+    private camera: CameraService,
+    private router: Router
+  ) {
     this.photos = this.camera.photos;
   }
-  
-  photos: String[] = []
+
+  photos: String[] = [];
+  public actionSheetButtons = [
+    {
+      text: 'Tomar foto',
+      role: 'destructive',
+      handler: () => {
+        this.takePhoto();
+      },
+    },
+    {
+      text: 'Este dispositivo',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 
   user: any = {};
   usrId = 0;
   users: any = [];
 
   ngOnInit() {
+    this.presentingElement = document.querySelector('.ion-page');
     this.activatedRoute.paramMap.subscribe((paramMap) => {
       const recipeId = paramMap.get('userId');
       console.log(recipeId);
-      this.usrId = parseInt(recipeId!, 10) -  1;
-      this.dataService.getUsers().subscribe(data =>{
+      this.usrId = parseInt(recipeId!, 10) - 1;
+      this.dataService.getUsers().subscribe((data) => {
         this.users = data;
         this.user = this.users[this.usrId];
-      })
-      
-     });
+      });
+    });
   }
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.activatedRoute.paramMap.subscribe((paramMap) => {
       const recipeId = paramMap.get('userId');
       console.log(recipeId);
       this.usrId = parseInt(recipeId!, 10) + 1;
-      this.dataService.getUsers().subscribe(data =>{
+      this.dataService.getUsers().subscribe((data) => {
         this.users = data;
         this.user = this.users[this.usrId];
-      })
-     });
+      });
+    });
   }
-  async takePhoto(){
+  async takePhoto() {
     this.camera.takePhoto();
   }
 }
-
