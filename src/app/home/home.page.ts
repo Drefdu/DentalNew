2cb2f6d848c31357c3bd984408c78a835a124cb1
Antonio <tonyestrada658@gmail.com>
2023-08-17 04:12:59 -0600

    cosas tony ya

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth, onAuthStateChanged } from 'firebase/auth';
 import { environment } from 'src/environments/environment';
 import { SessionService } from '../services/session.service';
+import { DataService } from 'src/app/services/data.service';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-home',
@@ -14,15 +16,21 @@ export class HomePage {
   user: any = {};
   app = initializeApp(environment.firebaseConfig)
   auth = getAuth();
+  datos:any={};
+
   provider = new GoogleAuthProvider();
 
-  constructor(private router: Router, private session: SessionService) {}
+  constructor(private router: Router, private session: SessionService,private dataService: DataService, private database: DatabaseService) { }
   
   ngOnInit() {
       onAuthStateChanged(this.auth, (user) => {
         if (user) {
           this.user = user;
-          console.log(this.user);
+          this.database.getUser(this.user.uid).subscribe(data => {
+            this.datos=data;
+            console.log(this.datos);
+          });
+          
         }
       });
   }
